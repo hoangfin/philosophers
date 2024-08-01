d9eddcb91692ce943f34963eeb15e8df77d670fc
Hoang Tran <hoangtran.fin@gmail.com>
2024-08-01 16:57:39 +0300

    Improved msleep: Removed unnecessary lock of state_mutex

--- philo_bonus/src/simulation/msleep_bonus.c
+++ philo_bonus/src/simulation/msleep_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 12:16:02 by hoatran           #+#    #+#             */
-/*   Updated: 2024/07/30 22:20:25 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/08/01 16:56:31 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,20 @@
 void	msleep(long duration, t_sim *sim)
 {
 	const long	start = now();
+	long		current_time;
+	long		max_duration;
 
-	while (1)
+	max_duration = start + duration;
+	if (sim != NULL)
+		max_duration = start + sim->time_to_die;
+	current_time = now();
+	while (current_time - start < duration && current_time < max_duration)
 	{
-		if (sim != NULL)
-		{
-			lock(sim->state_mutex, "msleep: lock");
-			if (sim->state != SIM_RUNNING)
-				return (unlock(sim->state_mutex, "msleep: unlock"));
-			unlock(sim->state_mutex, "msleep: unlock");
-		}
-		if (now() - start >= duration)
-			return ;
 		if (usleep(500) != 0)
 		{
 			write(2, "msleep: usleep\n", 15);
 			exit(1);
 		}
+		current_time = now();
 	}
 }
